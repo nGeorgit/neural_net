adc19003878a28440dbf7b6dc9128d2449051e90
nGeorgit <n.georgitsogiannis@gmail.com>
2024-07-07 20:13:37 +0300

    added tow feed forword functions

--- neural_net.py
+++ neural_net.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pprint import pprint
+import math
 
 class Neural:
     """
@@ -13,7 +14,33 @@ class Neural:
         self.weightLayers: list[np.ndarray] = []
         self.weightLayers.append(np.random.rand(self.inputSize, anodeLayers[0]))
         for i in range(len(anodeLayers)-1) :
-            self.weightLayers.append(np.random.rand(self.inputSize, anodeLayers[i+1]))
+            self.weightLayers.append(np.random.rand(anodeLayers[i], anodeLayers[i+1]))
+
+    def run(self, inputs: np.ndarray) -> np.ndarray:
+        activationVect: np.ndarray = inputs
+        for i in range(len(self.nodeLayers)):
+            #print(i)
+            #print(activationVect)
+            #print(self.weightLayers[i])
+            #print(np.matmul(activationVect, self.weightLayers[i]))
+            #print(self.nodeLayers[i].shape)
+            activationVect = self.activation_function(np.dot(activationVect, self.weightLayers[i]) + np.transpose(self.nodeLayers[i]))
+            pprint(activationVect)
+        return activationVect
+    
+    def feedforward(self, a):
+        """Return the output of the network if ``a`` is input."""
+        for b, w in zip(self.nodeLayers, self.weightLayers):
+            a = self.activation_function(np.dot(w, a)+b)
+        return a
+
+
+    def activation_function(self, z: np.ndarray) -> np.ndarray:
+        return self.sigmoid(z)
+
+    def sigmoid(self, z):
+        """The sigmoid function."""
+        return 1.0/(1.0+np.exp(-z))
 
     def print_nodeLayers(self) -> None:
         for v in self.nodeLayers: pprint(v)
@@ -21,6 +48,5 @@ class Neural:
     def print_weightLayers(self) -> None:
         for v in self.weightLayers: pprint(v)
 
-neural = Neural(2, [2,3])
-neural.print_nodeLayers()
-neural.print_weightLayers()
+neural = Neural(50, [10,10])
+pprint(neural.run(np.random.rand(1,50)))
